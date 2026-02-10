fce0a020476af213aff3487d9dec74ba3584a40f
samer moussa <samer.moussa@dcmail.ca>
2026-02-10 00:09:23 -0500

    Database 3ND draft
    drafted a 3ND database. Prepping to create a database to store all game data, and logs for better score tracking and data analysis. Seperated the project into multiple structures data, inputs, config, agent, and printer for now for better project comprehension

--- src/printer/score_run.py
+++ src/printer/score_run.py
@@ -57,3 +57,25 @@ def speed_run_fail_wrapper(printer_name, function):
             printer.cut()
             printer.close()
     
+def score_bar(score: int) -> str:
+    total_length = 24
+    score_str = f"{score} PTS"
+    
+    bar_length = total_length - len(score_str) - 1
+    
+    max_score = 250
+    
+    filled_length = int((min(score, max_score) / max_score) * bar_length)
+    
+    filled_length = min(filled_length, bar_length)
+    
+    empty_length = bar_length - filled_length
+    bar = "█" * filled_length
+    
+    if empty_length >= 3:
+        gradient = "▓▒░"
+        gradient = gradient[:empty_length]
+    else:
+        gradient = " " * empty_length
+    
+    return f"{bar}{gradient} {score_str}\n"
